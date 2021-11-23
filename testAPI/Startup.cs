9075f04f94f63c5affa9fa7ee117640a8a7e794a
Игорь Куксов <advisor@aspu.ru>
2021-11-23 17:41:24 +0400

    +hangfire

--- testAPI/Startup.cs
+++ testAPI/Startup.cs
@@ -1,8 +1,13 @@
+using Hangfire;
+using Hangfire.Mongo;
+using Hangfire.Mongo.Migration.Strategies;
+using Hangfire.Mongo.Migration.Strategies.Backup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 using TelegramAPI.Repository;
 using TelegramAPI.Repository.Impl;
 using testAPI.Attributes;
@@ -19,6 +24,31 @@ namespace testAPI
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var mongoUrlBuilder = new MongoUrlBuilder("mongodb://localhost:27017/Telegram");
+            var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+
+            // Add Hangfire services. Hangfire.AspNetCore nuget required
+            object p = services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMongoStorage(mongoClient, mongoUrlBuilder.DatabaseName, new MongoStorageOptions
+                {
+                    MigrationOptions = new MongoMigrationOptions
+                    {
+                        MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                        BackupStrategy = new CollectionMongoBackupStrategy()
+                    },
+                    Prefix = "hangfire.mongo",
+                    CheckConnection = true
+                })
+            );
+            // Add the processing server as IHostedService
+            services.AddHangfireServer(serverOptions =>
+            {
+                serverOptions.ServerName = "Hangfire.Mongo server 1";
+            });
 
             services.AddScoped<ITimeTableRepository, TimeTablesRepository>();
             services.AddControllersWithViews();
